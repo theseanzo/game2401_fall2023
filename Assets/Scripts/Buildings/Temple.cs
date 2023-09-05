cb9d96d5fc69b8b4b04e343418d4ac3358f9351c
Sean Lynch <seanzolynch@gmail.com>
2023-09-05 15:54:52 -0600

    Initial Commit
    Adding all of the files for the first lecture

---
+++ Assets/Scripts/Buildings/Temple.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temple : Building
+{
+   
+    Light templeRadiance;
+    float intensityLevel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+        templeRadiance = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        
+    }
+}

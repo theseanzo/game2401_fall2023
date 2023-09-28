14061dac210edb4253f104e52775b08fe3aba698
ShaylynKM <shaylynmelin@gmail.com>
2023-09-28 15:10:57 -0600

    Cone house
    cone

---
+++ Assets/Scripts/Buildings/smelin_Cone.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smelin_Cone : Building
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

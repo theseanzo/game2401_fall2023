2d743779faf188f54d6547d0247c4c37de5e7835
ShaylynKM <shaylynmelin@gmail.com>
2023-11-04 23:14:24 -0600

    Finished(*)
    Camera zoom:
    https://www.youtube.com/watch?v=HxnpWhxjJwE
    
    Camera rotate:
    https://www.youtube.com/watch?v=W70n_bXp7Dc
    (specifically the top comment)
    
    Transform.Rotation (helped me troubleshoot the camera moving in the wrong direction after rotating:)
    https://docs.unity3d.com/ScriptReference/Transform-rotation.html
    
    Frankensteined these together for panning:
    https://www.youtube.com/watch?v=uaiMvAK6Y0g
    https://discussions.unity.com/t/how-to-move-a-camera-only-using-the-arrow-keys/83318/2
    
    IsPointerOverGameObject:
    (where I found out about it)
    https://stackoverflow.com/questions/35604369/how-to-stop-camera-movement-when-interacting-with-ui-in-unity
    (Unity Documentation)
    https://docs.unity3d.com/2018.2/Documentation/ScriptReference/EventSystems.EventSystem.IsPointerOverGameObject.html
    
    (* This is as good as it's getting)

--- Assets/Scripts/CamControls.cs
+++ Assets/Scripts/CamControls.cs
@@ -9,12 +9,6 @@ public class CamControls : MonoBehaviour
     private Bounds bounds; // This variable was already here, so I'm assuming it's okay for me to use it the same way as we have been.
 
     [SerializeField]
-    private float _horizontalSpeed = 2f; // Horizontal rotation speed
-
-    [SerializeField]
-    private float _verticalSpeed = 2f; // Vertical rotation speed
-
-    [SerializeField]
     private float _zoomLevel; // The amount of zoom
 
     [SerializeField]
@@ -44,34 +38,40 @@ public class CamControls : MonoBehaviour
     private Vector3 _cameraPosition; // Camera's current position
 
     [SerializeField]
-    private float _panSpeed = 20; // How fast the camera pans
+    private float _panSpeed = 20f; // How fast the camera pans
 
     [SerializeField]
     private Camera _camera; // Reference to the camera
 
-    RaycastHit _hit;
+    [SerializeField]
+    private Vector3 _maxAngle; // Furthest the camera will rotate when zooming
+
+    [SerializeField]
+    private Vector3 _minAngle; // Smallest amount the camera will rotate when zooming
 
     void Start()
     {
         // Sets the zoom level to the orthographic size of the camera when the game starts
         _zoomLevel = _camera.fieldOfView;
 
-        // Set's the camera position variable to the current position
+        // Sets the cam pivot's position variable to the current position
         _cameraPosition = this.transform.position;
     }
 
+    // This stopped working after I added the part that changes the min and max angles with the zoom level. Changes whether the orbit works at all if Zoom() or Orbit() are called first. With Zoom() called first, orbitting works, but the angle immediately snaps back to 0 the moment I click. I'm pretty sure this is to do with interactions between the camera and its parent, but I'm running out of (reasonable) time and I'm going to have to call it here.
     private void Orbit()
     {
         // If we are holding down the right mouse button...
-        if(Input.GetMouseButton(1))
+        if (Input.GetMouseButton(1))
         {
             // Rotates the camera around the Y axis when moving the mouse from side to side
             // Removed the X rotation from the tutorial I followed and changed "Mouse Y" to "Mouse X" so that the camera rotates when moving the mouse side to side, not up and down.
             _yRotation += Input.GetAxis("Mouse X") * Time.deltaTime * _rotationSensitivity;
 
-            // This allows the camera's transform to be rotated in local space (I think, from reading the Unity Docs)
+            // This allows the cam pivot's transform to be rotated in local space (I think, from reading the Unity Docs)
             transform.localEulerAngles = new Vector3(_xRotation, _yRotation, 0);
         }
+
     }
 
     private void Pan()
@@ -105,11 +105,22 @@ public class CamControls : MonoBehaviour
 
         // Keeps camera within boundaries
         transform.position = bounds.ClosestPoint(transform.position);
-
     }
 
-    private void Zoom()
+    public void Zoom()
     {
+
+        // If the camera is zoomed all the way out, set the rotation to the max angle.
+        if (_zoomLevel == _zoomMax)
+        {
+            this.transform.rotation = Quaternion.Euler(_maxAngle);
+        }
+        // Else, set the rotation to the min angle.
+        else
+        {
+            this.transform.rotation = Quaternion.Euler(_minAngle);
+        }
+
         // References the scroll wheel
         float scroll = Input.GetAxis("Mouse ScrollWheel");
 
@@ -117,12 +128,12 @@ public class CamControls : MonoBehaviour
         // "The zoom multiplier is necessary so that we don't get tendonitis from scolling." - The guy whose tutorial I followed
         _zoomLevel -= scroll * _zoomMultiplier;
 
-        // Clamps the zoom between the min and max values so we don't zoom forever and ever and ever
+        // Clamps the zoom + angle between the min and max values so we don't zoom forever and ever and ever
         _zoomLevel = Mathf.Clamp(_zoomLevel, _zoomMin, _zoomMax);
 
         // Smooths the movement of the camera as it zooms in and out
         _camera.fieldOfView = Mathf.SmoothDamp(_camera.fieldOfView, _zoomLevel, ref _velocity, _smoothTime);
-        
+
     }
 
     void Update()
@@ -130,21 +141,9 @@ public class CamControls : MonoBehaviour
         // If the mouse is not hovered over a UI element:
         if(!EventSystem.current.IsPointerOverGameObject())
         {
-            Orbit();
             Pan();
             Zoom();
+            Orbit();
         }
-
-
-        // These are the old camera controls. I will be making new ones, using these as reference. - Shaylyn
-
-        ////we are going to simply move according to our mouse movement if we hold down the left mouse button
-        //if (Input.GetMouseButton(1)) //question: what is the difference between GetMouseButton and GetMouseButtonDown?
-        //{
-        //    float mouseXMovement = Input.GetAxis("Mouse X");
-        //    float mouseYMovement = Input.GetAxis("Mouse Y");
-        //    this.transform.Translate(-mouseXMovement, 0, -mouseYMovement);
-        //    transform.position = bounds.ClosestPoint(transform.position);
-        //}
     }
 }

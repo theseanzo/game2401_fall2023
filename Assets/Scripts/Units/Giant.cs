8d1e4bd2bec5e64f4e32b9cbf6293e434f345b2e
Blake Laprade <lapradeblake@gmail.com>
2023-11-29 12:08:27 -0700

    Assignment 4 WIP

---
+++ Assets/Scripts/Units/Giant.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Giant : Unit
+{
+
+    public override void OnAttackActionEvent()
+    {
+        base.OnAttackActionEvent();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

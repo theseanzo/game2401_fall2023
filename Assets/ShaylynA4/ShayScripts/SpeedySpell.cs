e0c60dcb7cf28e747d81afa5fd5e359c167749ad
ShaylynKM <shaylynmelin@gmail.com>
2023-12-04 00:01:48 -0700

    Offensive Spell Part 1 finished

--- Assets/ShaylynA4/ShayScripts/SpeedySpell.cs
+++ Assets/ShaylynA4/ShayScripts/SpeedySpell.cs
@@ -1,17 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SpeedySpell : MonoBehaviour
 {
-    public float Lifetime = 4f;
-
-    private void Update()
-    {
-        Destroy(this.gameObject, Lifetime); // Destroys the spell after a set period of time
-    }
-
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if(other.gameObject.GetComponent<Unit>())
         {

1bc09830b92bd1c9167476b4e976e44600ebcdb5
ShaylynKM <shaylynmelin@gmail.com>
2023-12-03 20:55:50 -0700

    Attack cat now spawns

--- Assets/ShaylynA4/ShayScripts/Catboy.cs
+++ Assets/ShaylynA4/ShayScripts/Catboy.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Catboy : Unit
 {
+    public GameObject Cat;
+
     public override void OnAttackActionEvent() // Overriding the parent's function
     {
         base.OnAttackActionEvent(); // Completes the parent's OnAttackActionEvent
         if (attackTarget != null) // Checks to make sure there is a target
         {
             attackTarget.OnHit(attackPower);
+
+            Instantiate(Cat, new Vector3(0, 0, 0), Quaternion.identity);
         }
     }
 }

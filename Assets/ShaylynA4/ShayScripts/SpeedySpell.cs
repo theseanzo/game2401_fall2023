329aea3fb0024a34cedc16c4935632b275a66823
ShaylynKM <shaylynmelin@gmail.com>
2023-12-06 03:10:42 -0700

    Spell working!!!!!
    I am so excited!!!!!

--- Assets/ShaylynA4/ShayScripts/SpeedySpell.cs
+++ Assets/ShaylynA4/ShayScripts/SpeedySpell.cs
@@ -5,16 +5,19 @@ using UnityEngine.Events;
 
 public class SpeedySpell : MonoBehaviour
 {
+    public int NewMoveSpeed = 10;
+
     private void OnTriggerEnter(Collider other)
     {
-        Building building = other.GetComponent<Building>();
+        Unit unit = other.GetComponent<Unit>(); // Reference to the Unit script
 
-        if (building != null) // If the object has the Building script
+        if (unit != null) // If the object is a Unit
         {
-            Destroy(other.gameObject); // This is just a test
+            unit.moveSpeed = NewMoveSpeed; // Makes the Units zoomier    
+        }
+        else
+        {
+            return;
         }
     }
 }
-
-// This works with the buildings, but not with any Units, even if I specify a certain type of Unit (like Soldier.) I also don't know how I'm supposed to modify the stats of the Unit since the setter is protected.
-// It works regardless of if I'm in Build or Attack mode (with the buildings.) The buildings come back after loading into a different mode (which is what I want, since I don't want the spell to last forever.)

82462a724427cc49f8961adb3a56fb6a9cbddb44
ShaylynKM <shaylynmelin@gmail.com>
2023-12-12 22:11:13 -0700

    Working particle system on building and Units

--- Assets/smelin/ShayScripts/Defence.cs
+++ Assets/smelin/ShayScripts/Defence.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Defence : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int NewMoveSpeed = 10;
+    public ParticleSystem Indicator;
+    public ParticleSystem Cone;
+    private bool _affected = false;
+
+    void Awake()
+    {
+        Cone = GetComponentInChildren<ParticleSystem>();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Unit unit = other.GetComponent<Unit>(); // Reference to the Unit script
+
+        if (unit != null) // If the object is a Unit
+        {
+            _affected = true;
+            Cone.Play();
+            
+        }
+        else
+        {
+            return;
+        }
+
+        if (_affected == true)
+        {
+            Instantiate(Indicator, unit.transform); // Instantiates the indicator particle effect
+            unit.transform.parent = Indicator.transform; // Parents it to the Unit
+        }
     }
 }

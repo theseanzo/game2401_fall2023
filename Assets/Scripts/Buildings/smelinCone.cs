69b8c8f7c866b20f1abd6dd5355b97e137179a92
ShaylynKM <shaylynmelin@gmail.com>
2023-12-15 15:12:01 -0700

    Fixed class name

--- Assets/Scripts/Buildings/smelinCone.cs
+++ Assets/Scripts/Buildings/smelinCone.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class smelin_Cone : Building
+public class smelinCone : Building
 {
     private void Awake()
     {

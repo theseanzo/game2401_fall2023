74a5bb8f3f99ccfe7a50bf3198987844e568c82f
ShaylynKM <shaylynmelin@gmail.com>
2023-12-15 16:53:03 -0700

    UPDATE: Fixed issue
    Trigger box was overlapping with other buildings and made it so that the building couldn't be placed. Added a sprite to indicate where the trigger box is.

--- Assets/smelin/ShayScripts/Defence.cs
+++ Assets/smelin/ShayScripts/Defence.cs
@@ -13,12 +13,10 @@ public class Defence : MonoBehaviour
 
     private bool _affected = false; // Whether or not the Units are affected by the building
 
-    [SerializeField]
-    private Transform _theCones;
-
     void Awake()
     {
         Cone = GetComponentInChildren<ParticleSystem>(); // Finds the Particle System
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -29,12 +27,11 @@ public class Defence : MonoBehaviour
         {
             _affected = true;
             Cone?.Play(); // Plays the Particle System on the building (as long as there is one)
-            transform.GetChild(0)?.gameObject.SetActive(true); // Sets the child of this object (the empty object holding the cone carousel) active (as long as it exists)
-            
+            transform.GetChild(0)?.gameObject.SetActive(true); // Sets the child of this object (the empty object holding the cone carousel) active (as long as it exists)      
+
             // The spinning cones have made the Unit dizzy!
             unit.moveSpeed = NewMoveSpeed; // He's struggling to move!
             unit.attackInterval = NewAttackInterval; // He can't focus on his attacks!
-            
         }
         else
         {
@@ -43,8 +40,9 @@ public class Defence : MonoBehaviour
 
         if (_affected == true)
         {
-            Instantiate(Indicator, unit.transform); // Instantiates the indicator particle effect
+            Instantiate(Indicator, unit.transform); // Instantiates the dizzy particle effect
             unit.transform.parent = Indicator.transform; // Parents it to the Unit
         }
+
     }
 }

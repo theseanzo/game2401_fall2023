ebdafaf3b277fe0431027bfc6f6c0cec9fd79f6d
eJyrVkpMTk4tLg7Jz07NU7JSMkwyMDJJNkwxMTU3MDUwM0wzTbJINDa1TDUySkpNSUmzSDFMNE4zNVSqBQAHcBEN <cansuguccg@gmail.com>
2023-12-17 15:30:47 -0500

    Laser Beam defense
    I created lases belong weapon and added laser script

--- Assets/Scripts/Buildings/Cansug_Building.cs
+++ Assets/Scripts/Buildings/Cansug_Building.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Cansug_Building : Building
 {
+   
     // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+       
     }
 }
---
+++ Assets/Scripts/Leaser.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Leaser : MonoBehaviour
+{
+    [SerializeField]
+    public LineRenderer lr;
+    public Transform leaser;
+    public float weapon;
+    public float speed=0.05f;
+    public float range = 50f;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lr.SetPosition(0, transform.position);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+            }
+        }
+        else lr.SetPosition(1, transform.forward * 5000);
+    }
+}

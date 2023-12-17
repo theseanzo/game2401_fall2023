ca32fa9e393059e1252705ddf06bac065a1611d7
t.dinreinwald512 <t.dinreinwald512@mybvc.ca>
2023-12-16 20:35:12 -0700

    a5 spike things

--- Assets/Scripts/Buildings/MagicTower.cs
+++ Assets/Scripts/Buildings/MagicTower.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MagicTower : Building
 {
+    
+    public GameObject spikePrefab; // Spike prefab to be instantiated
+    public AudioSource audioSource; // Audio source to play the sound effect
+    public AudioClip spawnSound; // Sound effect to play when a spike is spawned
+    public float spawnLength = 10f; // Length at which spikes are spawned
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +21,61 @@ public class MagicTower : Building
     {
         
     }
+    public void SpikeShoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3[] spikePositions = new Vector3[(int)spawnLength];
+            for (int i = 0; i < spawnLength; i++)
+            {
+                spikePositions[i] = new Vector3(i, 0, 0);
+            }
+            SpawnSpikes(spikePositions);
+
+
+    }   }
+       public void SpawnSpikes(Vector3[] spikePositions)
+       {
+        foreach (Vector3 position in spikePositions)
+        {
+            SpawnSpike(position);
+        }
+       }
+    private void SpawnSpike(Vector3 position)
+    {
+        // Instantiate the spike prefab at the specified position.
+        GameObject spikePrefab = Resources.Load<GameObject>("SpikePrefab");
+        GameObject spike = Instantiate(spikePrefab, position, Quaternion.identity);
+        // Play the spawn sound effect
+        audioSource.PlayOneShot(spawnSound);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+    
+       
+  
+
+
+
+
+
+
+
+
+
+
+  
+     
